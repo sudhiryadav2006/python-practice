44398eaa46f932f57294bddbe404e3b0cd4cef9f
Sudhir kumar Yadav <sudheerkumaryadav7081@gmail.com>
2025-07-30 22:47:32 +0530

    Add files via upload

---
+++ 1 day .py
@@ -0,0 +1,13 @@
+# Python ka sabse basic program
+print("Hello sudhir ")
+print("Welcome to Python!")
+print("Let's start my coding journey.")
+
+# output
+# Hello sudhir
+# Welcome to Python!
+# Let's start your coding journey.
+
+
+
+
