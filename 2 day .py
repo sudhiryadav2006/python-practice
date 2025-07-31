b5655cf8790a0052b98f35ca0a76563d1693b25f
Sudhir kumar Yadav <sudheerkumaryadav7081@gmail.com>
2025-07-31 21:59:15 +0530

    Update 2 day .py

--- 2 day .py
+++ 2 day .py
@@ -5,3 +5,27 @@ is_student = True
 print("Name:", name)
 print("Age:", age)
 print("Student:", is_student)
+
+#  Output:
+# Name: Sudhir
+# Age: 22
+# Student: True
+
+name = input("Enter your name: ")
+print("Hello", name)
+
+#  Output:
+# Enter your name: Sudhir
+# Hello Sudhir
+
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+# Convert to integers
+sum = int(num1) + int(num2)
+print("Total:", sum)
+
+# output
+# Enter first number: 5
+# Enter second number: 10
+# Total: 15
